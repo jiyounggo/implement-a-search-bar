394e42376e5bfdaa47da97e3f38ad1cd5bce8b6e
Malza0408 <parkjh04080@gmail.com>
2022-09-16 16:54:50 +0900

    Feat: 추가 최상위 index에 mui theme provider 설정 및 css reset

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
