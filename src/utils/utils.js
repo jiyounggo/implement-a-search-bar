106a29c1bb3f8874a001c3d8aa3d311b46c6329e
Malza0408 <parkjh04080@gmail.com>
2022-09-28 23:21:30 +0900

    Feat: 추가 검색어와 리스트의 결과물이 같은 부분이 있는 경우 bold 처리 해 주는 함수

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,8 @@
-import { List } from "../pages/main/searchBar/Input.style";
-
-export const checkWordBold = (key, compareWord, word) => {
+export const checkWordBold = (compareWord, word) => {
   const reg = new RegExp(`(${compareWord})`, "g");
   const parts = word.split(reg);
 
-  return (
-    <List key={key}>
-      {parts.map((part) =>
-        part.match(reg) ? <p style={{ fontWeight: "600" }}>{part}</p> : part
-      )}
-    </List>
+  return parts.map((part) =>
+    part.match(reg) ? <p style={{ fontWeight: "600" }}>{part}</p> : part
   );
 };
