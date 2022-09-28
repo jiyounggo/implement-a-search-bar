3a65d0539092adfa960d0594a54eb846010bbfc4
Malza0408 <parkjh04080@gmail.com>
2022-09-28 19:06:07 +0900

    Feat: 추가 Loading상태라면 리스트 목록 대신 검색중...을 표시

--- src/pages/main/searchBar/SearchedLists.js
+++ src/pages/main/searchBar/SearchedLists.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { List, Ul } from "./Input.style";
 
-function SearchedLists({ searchedData, isInputFocused }) {
+function SearchedLists({ searchedData, isInputFocused, loading }) {
   return (
     searchedData.length > 0 &&
     isInputFocused && (
       <Ul>
-        {searchedData.map((data) => (
-          <List key={data.sickCd}>{data.sickNm}</List>
-        ))}
+        {loading ? (
+          <List>검색중...</List>
+        ) : (
+          searchedData.map((data) => (
+            <List key={data.sickCd}>{data.sickNm}</List>
+          ))
+        )}
       </Ul>
     )
   );
