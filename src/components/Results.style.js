715b12c49a55bbb0714379d0eb8baf71bc14cf56
Malza0408 <parkjh04080@gmail.com>
2022-09-29 10:16:48 +0900

    Design: 추가 focusing된 리스트 background-color 부여

--- src/components/Results.style.js
+++ src/components/Results.style.js
@@ -1,13 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Li = styled.li`
-  /* border: 1px solid blue; */
   width: 400px;
   list-style: none;
   margin: 0.5rem 0 0.5rem 0;
 
-  &:hover {
-    background-color: skyblue;
-    color: blue;
-  }
+  background-color: ${(props) => (props.isFocus ? "lightgray" : "#fff")};
 `;
