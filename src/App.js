be3aa5abde5715e05b66d421767efe584a42d187
Malza0408 <parkjh04080@gmail.com>
2022-09-29 10:13:00 +0900

    Remove: 제거 불필요한 파일 및 코드

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import Search from './components/Search';
-// import SearchBar from './components/SearchBar';
-import './App.css';
+import React from "react";
+import Search from "./components/Search";
+import "./App.css";
 
 export default function App() {
   return (
